8a8e27304d05ef35beec15c527fb233206e1e93d
Alfarabx <alfarabi1074@gmail.com>
2024-09-27 20:05:07 +0700

    .

--- src/components/index.js
+++ src/components/index.js
@@ -1,15 +1,17 @@
-import Home from "./Home"
-import Product from "./Products"
-import Tips from "./Tips"
-import Banner from "./Banner"
-import Testimonials from "./Testimonials"
-import Gallery from "./Gallery"
-import CTA from "./CTA"
-import Contact from "./Contact"
-import Footer from "./Footer"
+import Navbar from "./navbar"
+import Home from "./home"
+import Product from "./products"
+import Tips from "./tips"
+import Banner from "./banner"
+import Testimonials from "./testimonials"
+import Gallery from "./gallery"
+import CTA from "./cta"
+import Contact from "./contact"
+import Footer from "./footer"
 import NotFound from "./not_found"
 
 export {
+    Navbar,
     Home,
     Product,
     Tips,
